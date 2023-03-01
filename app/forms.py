b93733587f2b5ed54d02b65785af56e12f119df7
Fnu Ashmika <ashmika.bv@gmail.com>
2023-02-28 19:34:16 -0600

    Day 2 Homework changes

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,30 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, IntegerField, DecimalField
 from wtforms.validators import DataRequired, EqualTo
 
 class RegisterForm(FlaskForm):
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password =PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password =PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords Must Match!!')])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class BlogForm(FlaskForm):
+    title= StringField('Title', validators=[DataRequired()])
+    blog = TextAreaField('Blog', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class CarForm(FlaskForm):
+    make= StringField('Make', validators=[DataRequired()])
+    model = StringField('Model', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
+    color = StringField('Color', validators=[DataRequired()])
+    price = DecimalField('Price', validators=[DataRequired()]) 
+    submit = SubmitField('Submit')    
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm, BlogForm, CarForm
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.jinja', title='Home') 
+    form = CarForm()
+    if form.validate_on_submit():
+        flash(f'Successfully submitted!!')  
+        return redirect('/')
+    return render_template('index.jinja', title='Home', car_form=form) 
 
 @app.route('/about')
 def about():
@@ -12,13 +16,25 @@ def about():
 
 @app.route('/blog')
 def blog():
-    return render_template('blog.jinja')
+    form = BlogForm()
+    return render_template('blog.jinja', blog_form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.jinja')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Successfully logged in!!')  
+        return redirect('/')
+    return render_template('login.jinja', login_form = form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        flash(f'Request to sign-up {form.username} successful!')
+        return redirect('/')
     return render_template('register.jinja', form=form)
+
+
+
+ 
